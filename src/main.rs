bb21ef6308fe45cf88cb771d952e1ddfb2b59559
EM <martynov@eis.utoronto.ca>
2024-10-19 03:33:09 -0400

    regex with args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use regex::Regex;
+use clap::{App,Arg};
 
 fn main() {
 
-    let search_term = "bo+k";
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+         .help("The pattern to search for ")
+         .takes_value(true)
+         .required(true))
+        .get_matches(); 
+
+        let pattern = args.value_of("pattern").unwrap();
+        let re = Regex::new(pattern).unwrap(); 
+
+    //let search_term = "bo+k";
     let quote = "\
     Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of
      pages?";
 
-    let re = Regex::new(search_term).unwrap(); 
-
+    
     for line in quote.lines() {
 
         match re.find(line) {
@@ -18,5 +30,10 @@ fn main() {
             None => (),
         }
 
+    }
+
+    let v = vec![1,2,3];
+    for n in v.iter().map(|x| x * x) {
+        println!("{}",n);
     }
 }
