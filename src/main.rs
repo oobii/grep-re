9b970b84b3b136d0d69c96a0dcd420761cf98317
EM <martynov@eis.utoronto.ca>
2024-10-19 23:04:27 -0400

    read from stdin or file

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,11 @@
 use regex::Regex;
 use clap::{App,Arg};
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
-
-    let f = File::open("readme.md").unwrap();
-    let reader = BufReader::new(f);
-
-    
 
     println!("----------------------------------------------------");
 
@@ -22,32 +18,46 @@ fn main() {
          .help("The pattern to search for ")
          .takes_value(true)
          .required(true))
+        .arg(Arg::with_name("input")
+         .help("File to read from ")
+         .takes_value(true)
+         .required(true))
         .get_matches(); 
 
         let pattern = args.value_of("pattern").unwrap();
         let re = Regex::new(pattern).unwrap(); 
 
+        let input = args.value_of("input").unwrap_or("-");
     //let search_term = "bo+k";
     
- //
-    let _quote= "\
-    Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
-    It is the same with books. What do we seek through millions of
-     pages?";
-
-
-    for line_ in reader.lines() {
-        
-        let line = line_.unwrap();
-        
-        // println!("{} ({} bytes long)",line, line.len());
-
-        match re.find(&line) {
-            Some(_) => println!("{}",line),
-            None => (),
+ 
+        if input == "-"  {
+            let stdin = io::stdin();
+            let reader1 = stdin.lock();
+            for line_ in reader1.lines() {
+                let line = line_.unwrap();
+                match re.find(&line) {
+                    Some(_) => println!("{}",line),
+                    None => (),
+                }
+            }
+        } else {
+            let f = File::open(input).unwrap();
+            let reader2 = BufReader::new(f);
+            
+            for line_ in reader2.lines() {
+                let line = line_.unwrap();
+                match re.find(&line) {
+                    Some(_) => println!("{}",line),
+                    None => (),
+                }
+            }
+    
         }
-    }
+        
+
+
+    
 
 
 
